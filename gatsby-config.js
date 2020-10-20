b01d6880447f1c3e0d8b9d473f7a3251a12ce0c6
dpip <dpip.nc@gmail.com>
2020-10-20 16:57:15 -0400

    intitial commit // using gatsby // includes react-core patternfly library

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,38 @@
+const topNav = [
+  {
+    // The text displayed on the item
+    title: "Documentation",
+    // The root path that enables/activates the item
+    rootPath: "/documentation",
+    // The page that is actually linked to when the item is clicked
+    path: "/documentation/about",
+  },
+]
+
+const sideNav = [
+  {
+    // shows this side nav on the home page
+    rootPath: "/",
+    nav: [
+      {
+        // The text displayed on the item
+        title: "Direct link",
+        // The page that is linked to
+        path: "/direct/link",
+      },
+      {
+        title: "Expandable link",
+        pages: [
+          {
+            title: "Expandable link",
+            path: "/expandable/link",
+          },
+        ],
+      },
+    ],
+  },
+]
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -25,6 +60,19 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: "gatsby-theme-patternfly",
+      options: {
+        // enable or disable the top nav (default true)
+        useTopNav: true,
+        // enable or disable the side navs (default true)
+        useSideNav: true,
+        // one or more top navigation definitions
+        topNav: topNav,
+        // one or more side navigation definitions
+        sideNav: sideNav,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,48 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import {
+  Brand,
+  Button,
+  Card,
+  CardActions,
+  CardHeaderMain,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  Split,
+  SplitItem,
+  Text,
+  TextContent,
+  TextVariants,
+} from "@patternfly/react-core"
+import { TimesIcon } from "@patternfly/react-icons"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <Card>
+      <CardHeader>
+        <CardHeaderMain>
+          <Brand
+            src="https://pf4.patternfly.org/assets/images/pf-logo-small.svg"
+            alt="Patternfly Logo"
+          />
+        </CardHeaderMain>
+        <CardActions>
+          <Button variant="plain">
+            <TimesIcon />
+          </Button>
+        </CardActions>
+      </CardHeader>
+      <CardTitle></CardTitle>
+      <CardBody></CardBody>
+      <CardFooter></CardFooter>
+    </Card>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
